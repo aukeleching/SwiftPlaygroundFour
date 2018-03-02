91b8cdeea5bf967a9b3f35ed7b1d1a43e4358f22
aukeleching <35237440+aukeleching@users.noreply.github.com>
2018-03-01 21:14:15 -1000

    1

--- SwiftPlaygroundFour.playground/Contents.swift
+++ SwiftPlaygroundFour.playground/Contents.swift
@@ -30,3 +30,34 @@ while i <= 10
 }
 
 
+//coding challenge 1
+
+var u = 1
+
+while u <= 3
+{
+print (u)
+    u = u+1
+    
+}
+
+//coding challenge 2
+
+for number in 1...10
+{
+    print(number)
+}
+
+var newSum = 10
+for number in 1...10
+{
+    newSum -1(number)
+    print(newSum)
+}
+
+
+
+
+
+
+
